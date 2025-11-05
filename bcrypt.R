6ffe50ebe56cb6ca5e891350168b0fa8d8b20b1f
Kyle Coombs <kyle.g.coombs@gmail.com>
2025-11-05 18:33:30 -0500

    encoding

--- bcrypt.R
+++ bcrypt.R
@@ -8,3 +8,11 @@ roster <- read_csv('general-resources/final_question_reveal/roster.csv') %>%
     mutate(pw_hash=bcrypt::hashpw(password)) %>%
     select(-password) %>%
     write_csv("general-resources/final_question_reveal/credentials.csv")
+
+txt <- readChar("credentials.csv", file.info("credentials.csv")$size, useBytes = TRUE)
+txt_b64 <- base64enc::base64encode(charToRaw(txt))
+# add to Renviron
+# Sys.setenv(CRED_B64=txt_b64)
+cred_b64 <- Sys.getenv("CRED_B64")
+cred_txt <- rawToChar(base64enc::base64decode(cred_b64))
+cred_df <- read_csv(cred_txt)
